d1ddfc3848f23413f2052c85c4f5a0f7320ace62
aaronhoskins2011 <aaronhoskins2011@gmail.com>
2017-08-20 20:05:26 -0500

    Set menu select options

--- app/src/main/java/com/example/aaron/weekendprojectone/MainActivity.java
+++ app/src/main/java/com/example/aaron/weekendprojectone/MainActivity.java
@@ -4,13 +4,14 @@ import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
-import android.view.Menu;
 import android.view.MenuItem;
 
 
@@ -20,11 +21,13 @@ public class MainActivity extends AppCompatActivity {
     private ListView drawerList;
     private ArrayAdapter<String> mAdapter;
     private Menu menu;
+    RelativeLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainLayout = (RelativeLayout)findViewById(R.id.mainLayout);
 
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
@@ -40,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case 1:
                         addSecondActivity(view);
-                        Toast.makeText(MainActivity.this, "Activity 2", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Starting Media Player", Toast.LENGTH_SHORT).show();
                         break;
                     case 2:
                         Toast.makeText(MainActivity.this, "Activity 3", Toast.LENGTH_SHORT).show();
@@ -55,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addDrawerItems(){
-        String[] programTitles = {"EMI Calculator", "Activity 2" , "Activity 3"};
+        String[] programTitles = {"EMI Calculator", "Media Player" , "Activity 3"};
         mAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 ,programTitles);
         drawerList.setAdapter(mAdapter);
 
@@ -67,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void addSecondActivity(View view){
-        Intent intent = new Intent(this, Second_Activity.class);
+        Intent intent = new Intent(this, Media_Activity.class);
         intent.setAction("starting emi");
         startActivity(intent);
 
@@ -80,6 +83,29 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
     public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.actBackgroundOne:
+                Log.d("MENU", "onOptionsItemSelected: BackgoundOne");
+                mainLayout.setBackgroundResource(R.drawable.background_one);
+                break;
+            case R.id.actBackgroundTwo:
+                Log.d("MENU", "onOptionsItemSelected: BackgroundTwo");
+                mainLayout.setBackgroundResource(R.drawable.background_two);
+                break;
+            case R.id.actBackgroundDefualt:
+                Log.d("MENU", "onOptionsItemSelected: BackgroundDefault");
+                mainLayout.setBackgroundResource(R.drawable.default_backround);
+                break;
+        }
+
+
+
+
+
+
+
+
         return true;
     }
 
